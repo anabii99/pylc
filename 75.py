db56362d20e620c8f5595cdd8a206e5b81609869
anabii99 <abrorabyyu@duck.com>
2024-06-12 10:52:10 +0700

    update for pattern matching example

---
+++ 75.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        i, j, k = 0, 0, len(nums) - 1
+        while j <= k:
+            if nums[j] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+            elif nums[j] == 2:
+                nums[j], nums[k] = nums[k], nums[j]
+                k -= 1
+            else:
+                j += 1
+
+# Example usage:
+nums = [2,0,2,1,1,0]
+solution = Solution()
+solution.sortColors(nums)
+print(nums)  # Output: [0, 0, 1, 1, 2, 2]
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Some notes
 - 2.py, had some function that take vector into node
+- 75, it is implementation pattern matching from rust
